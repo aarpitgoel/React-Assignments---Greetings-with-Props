7dbd2f05d342f84aaf9376334a8f669caa8c9f13
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-11-08 23:24:05 +0530

    updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ export default function Welcome(props) {
   const name = props.name;
   return (
     <>
-      <h1>Hey !+ {name} </h1>
+      <h1>"Hey !"+ {name} </h1>
       <h2>"Welcome to Newton School."</h2>
     </>
   );
