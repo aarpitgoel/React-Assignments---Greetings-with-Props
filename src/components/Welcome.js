7051ccae8a36564f4397ac83b9e74549ec009dbf
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-11-08 23:20:37 +0530

    updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from "react";
+
+export default function Welcome(props) {
+  const name = props.name;
+  return (
+    <>
+      <h1>Hey !+ {name} </h1>
+      <h2>"Welcome to Newton School."</h2>
+    </>
+  );
+}
